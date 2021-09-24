0727e4721966ffb449096aa6a6ad707151ab3676
Harvey-46904 <harveympv@hotmail.com>
2021-09-24 16:20:36 -0500

    sprint2 dashboard

---
+++ database/migrations/2021_09_24_200826_create_clientes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipo_documento');
+            $table->string('numero_documento');
+            $table->string('nombres');
+            $table->string('apellidos');
+            $table->string('direccion');
+            $table->string('telefono');
+            $table->string('conductor_adicional');
+            $table->string('documento_conductor_adicional');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('clientes',"ClientesController@store");
+
+
+Route::get('dashboard', function () {
+    return view('dashboards.index');
+});
+
+Route::get('registro', function () {
+    return view('dashboards.registro_clientes');
+});
