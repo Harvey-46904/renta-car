2f951c07680b8cad2cb8a1156f3acae836f97097
Harvey-46904 <harveympv@hotmail.com>
2021-10-01 11:46:07 -0500

    update swet alert

--- resources/views/dashboards/lista_clientes.blade.php
+++ resources/views/dashboards/lista_clientes.blade.php
@@ -23,6 +23,7 @@
                                         <th>TELÉFONO</th>
                                         <th>NOMBRE CONDUCTOR ADICIONAL</th>
                                         <th>DOCUMENTO CONDUCTOR ADICIONAL</th>
+                                        <th>OPCIONES</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -36,6 +37,7 @@
                                         <td>3180987654</td>
                                         <td>HARVEY RIASCOS</td>
                                         <td>12987654</td>
+                                        <td><button type="button" class="btn btn-danger" id="btn_eliminar">eliminar</button></td>
                                     </tr>
                                 </tbody>
                             </table>
