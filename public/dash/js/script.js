17c718e974eb0fc547cb7d2617a082637863d4f7
Harvey-46904 <harveympv@hotmail.com>
2021-10-25 09:55:46 -0500

    reservas nuevas 1

--- public/dash/js/script.js
+++ public/dash/js/script.js
@@ -6,12 +6,25 @@ $('#cedula_usuario').keyup(function() {
     $.get( "obtener_nombre/"+texto, function( data ) {
        
         if(data.data =="error"){
-            $("#usuario").val("existe");
+            $("#mensaje").val("Cliente No Encontrado");
+            $("#mensaje").removeClass("bg-info")
+            $("#mensaje").addClass("bg-warning")
         }else{
+            $("#mensaje").val("Cliente Encontrado");
+          
+            asignar_datos_cliente(data.data.id_cliente,data.data.numero_documento,data.data.nombres,data.data.apellidos,data.data.direccion,data.data.telefono)
+            $("#mensaje").removeClass("bg-warning")
+            $("#mensaje").addClass("bg-success")
            
-            $("#usuario").val(data.data.nombres+" "+data.data.apellidos);
         }
       });
-
-    
    });
+
+   function asignar_datos_cliente(id,cedula,nombre,apellidos,direccion,telefono){
+    $("#id").val(id); 
+    $("#documento").val(cedula);
+    $("#nombres").val(nombre);
+    $("#apellidos ").val(apellidos);
+    $("#direccion").val(direccion);
+    $("#telefono").val(telefono);
+   }
--- resources/views/dashboards/crear_reserva.blade.php
+++ resources/views/dashboards/crear_reserva.blade.php
@@ -24,23 +24,96 @@
                             </ul>
                         </div>
                         <div class="body">
+
                             <form method="POST" action="{{ route('post_vehiculo') }}" accept-charset="UTF-8"
                                 enctype="multipart/form-data">
                                 @csrf
-                                <label for="nombre_vehiculo">No Documento</label>
-                                <div class="form-group">
-                                    <div class="form-line">
-                                        <input type="text" id="cedula_usuario" name="cedula_usuario" class="form-control"
-                                            placeholder="Digite numero de documento">
+                                <div class="row">
+                                    <div class="col-md-6"><label for="nombre_vehiculo">No Documento</label>
+                                        <div class="form-group">
+                                            <div class="form-line">
+                                                <input type="text" id="cedula_usuario" name="cedula_usuario"
+                                                    class="form-control" placeholder="Digite numero de documento">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="form-line">
+                                                <input type="text" id="mensaje" name="mensaje"
+                                                    class="form-control bg-warning" placeholder="Cliente No Encontrado">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 text-center border border-secondary py-2">
+
+                                        <h3 class="text-center">Ticket de Reserva</h3>
+                                        <hr>
+                                        <div class="row justify-content-center">
+                                            <div class="col-md-5">
+                                                <label for="nombre_vehiculo">Id Cliente</label>
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="text" id="id" name="id" class="form-control bg-light "
+                                                            placeholder="" value="" disabled>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-5">
+                                                <label for="nombre_vehiculo">No Documento</label>
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="text" id="documento" name="documento"
+                                                            class="form-control bg-light" placeholder="" value="" disabled>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row justify-content-center">
+                                            <div class="col-md-5">
+
+
+                                                <label for="nombre_vehiculo">Nombres</label>
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="text" id="nombres" name="nombres"
+                                                            class="form-control bg-light " placeholder="" value="" disabled>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-5">
+                                                <label for="nombre_vehiculo">Apellidos</label>
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="text" id="apellidos" name="apellidos"
+                                                            class="form-control bg-light" placeholder="" value="" disabled>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row justify-content-center">
+                                            <div class="col-md-5">
+                                                <label for="nombre_vehiculo">Dirección</label>
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="text" id="direccion" name="direccion"
+                                                            class="form-control bg-light " placeholder="" value="" disabled>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-5">
+                                                <label for="nombre_vehiculo">Telefono</label>
+                                                <div class="form-group">
+                                                    <div class="form-line">
+                                                        <input type="text" id="telefono" name="telefono"
+                                                            class="form-control bg-light" placeholder="" value="" disabled>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
                                     </div>
                                 </div>
-                                <label for="nombre_vehiculo">Cliente</label>
-                                <div class="form-group">
-                                    <div class="form-line">
-                                        <input type="text" id="usuario" name="usuario" class="form-control bg-success"
-                                            placeholder="" disabled>
-                                    </div>
-                                </div>
+
+
                                 <br>
                                 <button type="submit" class="btn btn-primary m-t-15 waves-effect">GUARDAR</button>
                             </form>
