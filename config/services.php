9dd244f3e835e5b8586e5116b58fc0fa759dcde8
Harvey-46904 <harveympv@hotmail.com>
2022-03-31 15:33:24 -0500

    nuevo fort fiesta

--- config/services.php
+++ config/services.php
@@ -30,4 +30,15 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'payu' =>[
+        'base_uri' => env('PAYU_BASE_URI'),
+        'account_id' => env('PAYU_BASE_ACCOUNT_ID'),
+        'merchant_id' => env('PAYU_BASE_MERCHANT_ID'),
+        'key' => env('PAYU_KEY'),
+        'secret' => env('PAYU_SECRET'),
+        'base_currency' => 'cop',
+        'class' => App\Services\PayUService::class,
+    ],
+
+
 ];
--- resources/views/webpage/inicio.blade.php
+++ resources/views/webpage/inicio.blade.php
@@ -354,6 +354,24 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <div class="col-lg-4">
+                                        <div class="single-offers">
+                                            <div class="offer-image"><a href="#"><img src="{!! asset('webpage/img/ford-fiesta.png')!!}"
+                                                        alt="offer 1" /></a></div>
+                                            <div class="offer-text"><a href="#">
+                                                    <h3>Ford Fiesta</h3>
+                                                </a>
+                                                <h4>$220.000<span>/ Día</span></h4>
+                                                <ul>
+                                                    <li><i class="fa fa-car"></i>Full equipo AC</li>
+                                                </ul>
+                                                
+                                                <div class="offer-action"><a href="" onclick="mensaje_wpp_carro('Ford Fiesta')" class="offer-btn-1">Reservar</a><a
+                                                        href="#" class="offer-btn-2">Detalles</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                   
                                     
                                 </div>
